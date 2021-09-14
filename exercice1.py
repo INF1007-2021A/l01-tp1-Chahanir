5326da9268d9b3c29f3cea19cd38f4fbbd5b8e4f
Chahine <tchamitouche@gmail.com>
2021-09-14 11:53:22 -0400

    nouvelle version

--- exercice1.py
+++ exercice1.py
@@ -1,10 +1,22 @@
 def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat
-
-    resultat =
+    resultat = ""
     return resultat
+
 
 if __name__ == '__main__':
     n = int(input("indiquez le nombre: "))
     print(fizzBuzz(n))
+    if n % 3 == 0:
+        if n % 5 ==0:
+            print('fizzbuzz')
+
+        else:
+            print('fizz')
+    elif n % 5 == 0:
+        print('buzz')
+    else:
+        print ('ledit')
+
+
